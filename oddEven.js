34c6ed1e1fe722f1cd99d55ef8c11e1499b7a0d3
Martun <65818128+martun-avagyan@users.noreply.github.com>
2021-12-02 14:12:04 +0400

    feat: completed oddEven problem with if statement

---
+++ oddEven.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let n = 125;
+
+if (n % 2 == 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
+
+n = 35;
+
+if (n % 2 == 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
+
+n = 20;
+
+if (n % 2 == 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
